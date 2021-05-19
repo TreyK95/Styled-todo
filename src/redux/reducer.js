e89f5b71db29c06435d3ca5f38f5508561dfa676
Trey Kemp <kemptrey@outlook.com>
2021-05-19 11:22:57 -0600

    added update and complete reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -12,8 +12,33 @@ const addTodoReducer = createSlice({
       state.push(action.payload);
       return state;
     },
+    removeTodos: (state, action) => {
+      return state.filter((item) => item.id !==action.payload)
+    }
+    updateTodos: (state, action) => {
+      return state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            item: action.payload.item,
+          }
+        }
+        return todo;
+      })
+    }
+    completeTodos: (state, action) => {
+      return state.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            completed:true,
+          }
+        }
+        return todo;
+      })
+    }
   },
 });
 
-export const { addTodos } = addTodoReducer.actions;
+export const { addTodos, removeTodos, updateTodos, completeTodos } = addTodoReducer.actions;
 export const reducer = addTodoReducer.reducer;
