b5b7836449eb91a1f587c4c36e20dd27d8ebf9ca
Trey Kemp <kemptrey@outlook.com>
2021-05-19 11:49:06 -0600

    displayTodo component done

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -13,32 +13,33 @@ const addTodoReducer = createSlice({
       return state;
     },
     removeTodos: (state, action) => {
-      return state.filter((item) => item.id !==action.payload)
-    }
+      return state.filter((item) => item.id !== action.payload);
+    },
     updateTodos: (state, action) => {
       return state.map((todo) => {
         if (todo.id === action.payload.id) {
           return {
             ...todo,
             item: action.payload.item,
-          }
+          };
         }
         return todo;
-      })
-    }
+      });
+    },
     completeTodos: (state, action) => {
       return state.map((todo) => {
         if (todo.id === action.payload) {
           return {
             ...todo,
-            completed:true,
-          }
+            completed: true,
+          };
         }
         return todo;
-      })
-    }
+      });
+    },
   },
 });
 
-export const { addTodos, removeTodos, updateTodos, completeTodos } = addTodoReducer.actions;
+export const { addTodos, removeTodos, updateTodos, completeTodos } =
+  addTodoReducer.actions;
 export const reducer = addTodoReducer.reducer;
